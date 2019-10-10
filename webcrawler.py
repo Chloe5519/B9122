9be8198560d74f463a5272b18955166078ebce3a
孙嘉澍 <sunjs@sunjiashudeMacBook-Pro.local>
2019-10-10 16:51:50 -0400

    add webcrawler.py file

---
+++ webcrawler.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+seed_url = "http://www8.gsb.columbia.edu"
+
+urls = [seed_url]    #queue of urls to crawl
+seen = [seed_url] # stack of urls seen so far
+opened = []
+
+
+maxNumUrl = 10; #set the maximum number of urls to visit
+print("Starting with url="+str(urls))
+while len(urls) > 0 and len(opened) < maxNumUrl:
+    # DEQUEUE A URL FROM urls AND TRY TO OPEN AND READ IT
+    try:
+        curr_url=urls.pop(0)
+        print("num. of URLs in stack: %d " % len(urls))
+        webpage=urllib.request.urlopen(curr_url)
+        opened.append(curr_url)
+
+    except Exception as ex:
+        print(ex)
+        continue    #skip code below
+
+    # IF URL OPENS, CHECK WHICH URLS THE PAGE CONTAINS
+    # ADD THE URLS FOUND TO THE QUEUE url AND seen
+    soup = BeautifulSoup(webpage)  #creates object soup
+    # Put child URLs into the stack
+    for tag in soup.find_all('a', href = True): #find tags with links
+        childUrl = tag['href']          #extract just the link
+        o_childurl = childUrl
+        childUrl = urllib.parse.urljoin(seed_url, childUrl)
+        print("url=" + seed_url)
+        print("original childurl=" + o_childurl)
+        print("childurl=" + childUrl)
+        print("url in childUrl=" + str(seed_url in childUrl))
+        print("childUrl not in seen=" + str(childUrl not in seen))
+        if seed_url in childUrl and childUrl not in seen:
+            print("***urls.append and seen.append***")
+            urls.append(childUrl)
+            seen.append(childUrl)
+        else:
+            print("######")
+
+print("num. of URLs seen = %d, and scanned = %d" % (len(seen), len(opened)))
+
+print("List of seen URLs:")
+for seen_url in seen:
+    print(seen_url)
